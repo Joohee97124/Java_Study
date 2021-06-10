5d2dff53b403c9342c9569c3b3c124a73166ff3d
[Joohee97124] <asdqorrn1@naver.com>
2021-06-10 22:55:35 +0900

    20210610

---
+++ SpringPrj02/src/com/test/spr/Record.java
@@ -0,0 +1,25 @@
+/*======================
+ 	Record.java
+ 	- 인터페이스
+======================*/
+
+package com.test.spr;
+
+public interface Record
+{
+	public void setKor(int kor);
+	public int getKor();
+	
+	public void setEng(int eng);
+	public int getEng();
+	
+	public void setMat(int mat);
+	public int getMat();
+	
+	public int getTotal();
+	
+	public double getAvg();
+	
+	
+	// 선언된 속성 없고, setter getter 만 설정 → 인터페이스라
+}
